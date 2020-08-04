471560ee08e2653b1f08ec3698547a4d4f47275e
Sadique Ilyas <sadiqueilyasfifa@gmail.com>
2020-08-04 16:22:50 +0530

    [Notes]

---
+++ app/src/main/java/com/example/mynotesapp/SplashScreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.mynotesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private ImageView splashImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        splashImage = findViewById(R.id.splashImage);
+        YoYo.with(Techniques.Tada).duration(2000).delay(200).playOn(splashImage);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+
+
+    }
+}
